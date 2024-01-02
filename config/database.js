cfa2784ddece4f9ef5560be0003201f35077004c
Muhammad Asim <Asim12@users.noreply.github.com>
2024-01-03 02:13:59 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 function connectionDatabase() {
     return new Promise((resolve, reject) => {
-        let url='mongodb+srv://mohsinraza:M.hsnnss1@findcluster.jqsni.mongodb.net/fitness-hub?retryWrites=true&w=majority';
+        let url='';
         MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, async(err, client) => {
             if (err) {
                 // db.close();
